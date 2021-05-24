8ef0b7236bec2764b3dd559021e5985ab4b162ef
bharath9324 <bharath9324@gmail.com>
2021-05-24 04:47:40 +0000

    Update docker wrap

--- docker-wrap.sh
+++ docker-wrap.sh
@@ -4,11 +4,9 @@ if [ "$1" == "build" ]; then
 elif [ "$1" == "prune" ]; then
 	docker system prune -a
 elif [ "$1" == "run" ]; then
-	docker run -it -d -p 8000:8000 --name dev django-image /bin/bash
+	docker run -it -d -p 8000:8000 -v ~/.gitconfig:/etc/gitconfig --name dev django-image /bin/bash git config --list
 elif [ "$1" == "attach"	 ]; then
 	docker attach dev
 else
 	printf "Valid arguments are \n build\n prune\n run\n attach\n"
 fi
-
-
