0d9b400323a116caf65e944740aeb5a4c8d92464
Your Name <you@example.com>
2021-05-24 03:59:26 +0000

    Update docker wrap

--- docker-wrap.sh
+++ docker-wrap.sh
@@ -3,6 +3,12 @@ if [ "$1" == "build" ]; then
 	docker build --tag django-image .
 elif [ "$1" == "prune" ]; then
 	docker system prune -a
+elif [ "$1" == "run" ]; then
+	docker run -it -d -p 8000:8000 --name dev django-image /bin/bash
+elif [ "$1" == "attach"	 ]; then
+	docker attach dev
 else
-	printf "Valid arguments are \n build\n prune\n"
+	printf "Valid arguments are \n build\n prune\n run\n attach\n"
 fi
+
+
